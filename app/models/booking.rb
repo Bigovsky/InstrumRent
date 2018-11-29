02089639b1479f7dc94457145f69bf7f0f5c3144
Abdelilah El hotmani <abdelilah.elhotmani@outlook.com>
2018-11-29 11:08:23 +0100

    Rectifying some booking logics

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@
   belongs_to :user
 
 
+  validate :bookings_must_not_overlap
   validates :instrument, presence: true
   validates :user, presence: true
   validates :start_date, presence: true
@@ -10,15 +11,27 @@
   validate :start_date_cannot_be_in_the_past
   validate :end_date_cannot_be_in_the_past
 
-  def start_date_cannot_be_in_the_past
-    if start_date.present? && start_date < Date.today
+    def start_date_cannot_be_in_the_past
+      if start_date.present? && start_date < Date.today
       errors.add(:start_date, "can't be in the past")
+      end
     end
-  end
 
-  def end_date_cannot_be_in_the_past
-    if end_date.present? && end_date < Date.today
+    def end_date_cannot_be_in_the_past
+      if end_date.present? && end_date < Date.today
       errors.add(:end_date, "can't be in the past")
+      end
     end
-  end
+
+    private
+      def bookings_must_not_overlap
+        return if self
+              .class
+              .where.not(id: id)
+              .where(instrument_id: instrument_id)
+              .where('start_date < ? AND end_date > ?', end_date, start_date)
+              .none?
+          errors.add(:base, 'this instrument is booked on those dates')
+      end
 end
+
